9ef87ef7f35bed59ce63392fbc63d6df03a0a4bd
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-08-14 06:02:56 +0600

    added server side api/products to get single product by id or all products

---
+++ app/api/products/route.js
@@ -0,0 +1,22 @@
+import { mongooseConnect } from "@/Database/mongoose";
+import { Product } from "@/models/Product";
+
+export const GET = async(req, res) =>{
+    const params = new URL(req.url).searchParams
+    const id = params.get("id")
+    try{
+        await mongooseConnect()
+        let products;
+        
+        if(id){
+            products = await Product.findById(id)
+        }else{
+            products = await Product.find()
+        }
+        
+        
+        return new Response(JSON.stringify(products), {status: 200})
+    }catch(error){
+        console.log("error in api/products/get *** ", error)
+    }
+}
--- app/page.js
+++ app/page.js
@@ -1,8 +1,15 @@
 "use client"
 import Featured from "@/components/Featured"
 import Header from "@/components/Header"
+import axios from "axios"
+import { useEffect } from "react"
 
 export default function Home() {
+  useEffect(() => {
+    axios.get("/api/products?id=" + "64d7d96d55a7e77ee46cb733")
+    .then(result => console.log(result))
+  }, [])
+  
   return (
     <div>
       <Header />
