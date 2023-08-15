93c945de355505ef18f657faf0a51526f4fa8d6b
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-08-15 08:46:05 +0600

    sending products info and users's address and all to backend to processed and sent it to strype payment process

---
+++ app/api/checkout/route.js
@@ -0,0 +1,38 @@
+import { mongooseConnect } from "@/Database/mongoose"
+import { Product } from "@/models/Product"
+
+export const POST = async(req, res) =>{
+    try{
+
+        const {name, email, city, postalCode, streetAddress, country, products} = await req.json()
+        
+        await mongooseConnect()
+        const productIds = products.split(",")
+        const uniqueIds = [...new Set(productIds)]
+        
+        const productInfos = await Product.find({_id:uniqueIds})
+        
+        let line_items = []
+        
+        for (const productId of uniqueIds) {
+            const productInfo = productInfos.find(p => p._id.toString() === productId)
+
+            const quantity = productIds.filter(id => id === productId)?.length || 0
+            
+            if (quantity > 0 && productInfo){
+                line_items.push({
+                    quantity,
+                    price_data: {
+                        currency: "USD",
+                        product_data: {name: productInfo.title},
+                        unit_amount: quantity * productInfo.price,
+                    }
+                });
+            }
+        }
+
+        return new Response(JSON.stringify(line_items), {status: 200})
+    }catch(error){
+        console.log("error ***: ", error)
+    }
+}
--- app/cart/page.js
+++ app/cart/page.js
@@ -3,6 +3,7 @@ import Button from '@/components/Button';
 import { CartContext } from '@/components/CartContext';
 import Center from '@/components/Center';
 import Header from '@/components/Header'
+import Input from '@/components/Input';
 import Table from '@/components/table';
 import axios from 'axios';
 import Image from 'next/image';
@@ -20,6 +21,7 @@ const Box = styled.div`
     background-color: #fff;
     border-radius: 10px;
     padding: 30px;
+    padding-top: 0;
 `;
 
 const ProductInfoCell = styled.td`
@@ -53,15 +55,32 @@ const QunatityLable = styled.span`
     padding: 0 3px;
 `;
 
+
+const CityHolder = styled.div`
+    display: flex;
+    gap: 5px;
+`
+
+
 const CartPage = () => {
     const { cartProducts, removeCartProduct, addProductToCart } = useContext(CartContext)
 
     const [products, setProducts] = useState([])
 
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [city, setCity] = useState("")
+    const [postalCode, setPostalCode] = useState("")
+    const [streetAddress, setStreetAddress] = useState("")
+    const [country, setCountry] = useState("")
+
+
     useEffect(() => {
         if (cartProducts.length) {
             axios.post("/api/cart", { ids: cartProducts })
                 .then(response => setProducts(response.data))
+        } else {
+            setProducts([])
         }
     }, [cartProducts])
 
@@ -78,6 +97,13 @@ const CartPage = () => {
     for (const productId of cartProducts) {
         const price = products.find(p => p._id === productId)?.price || 0
         total += price;
+    }
+
+    function handleFormSubmit(e){
+        e.preventDefault()
+        axios.post("/api/checkout", {name, email, city, postalCode, streetAddress, country, products: e.target.products.value})
+        .then(response => console.log(response.data))
+
     }
 
     return (
@@ -156,10 +182,48 @@ const CartPage = () => {
                         <Box>
                             <h2> Order Information</h2>
 
-                            <input type="text" placeholder='Address' />
-                            <input type="text" placeholder='Address 2' />
+                            <form onSubmit={handleFormSubmit}>
+                                <Input type="text"
+                                    placeholder='Name'
+                                    value={name}
+                                    name="name"
+                                    onChange={e => setName(e.target.value)}/>
+                                <Input type="email"
+                                    placeholder='Email'
+                                    value={email}
+                                    name="email"
+                                    onChange={e => setEmail(e.target.value)} />
 
-                            <Button $black $block >Continue to payment</Button>
+                                <CityHolder>
+                                    <Input type="text"
+                                        placeholder='City'
+                                        value={city}
+                                        name="city"
+                                        onChange={e => setCity(e.target.value)} />
+                                    <Input type="text"
+                                        placeholder='Postal Code'
+                                        value={postalCode}
+                                        name="postalCode"
+                                        onChange={e => setPostalCode(e.target.value)} />
+                                </CityHolder>
+
+                                <Input type="text"
+                                    placeholder='Street Address' 
+                                    value={streetAddress}
+                                    name="streetAddress"
+                                    onChange={e => setStreetAddress(e.target.value)} />
+                                <Input type="text"
+                                    placeholder='Country'
+                                    value={country}
+                                    name="country"
+                                    onChange={e => setCountry(e.target.value)} />
+
+                                <input type="hidden" 
+                                    value={cartProducts.join(",")} 
+                                    name="products"/>
+
+                                <Button $black $block type="submit">Continue to payment</Button>
+                            </form>
                         </Box>
                     )}
                 </ColumsWrapper>
